c88dbcb54359d694aefd85631fba6eb931ecc62c
EliaBR <felipe.elia@gmail.com>
2018-07-18 17:49:00 -0300

    [#1] Cria as roles e capabilities do site

--- themes/lemann-lideres/functions.php
+++ themes/lemann-lideres/functions.php
@@ -1,4 +1,8 @@
 <?php
+
+// Cria as roles e capabilities personalizadas do site
+require get_stylesheet_directory() . '/includes/roles-capabilities.php';
+
 add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/assets/css/style.css' );
---
+++ themes/lemann-lideres/includes/roles-capabilities.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Cria as roles e capabilities personalizadas do site.
+ */
+
+function lemann_roles_capabilities() {
+	global $wp_roles;
+
+	add_role( 'lider', 'Líder', array(
+		'read'                    => true,
+		'edit_posts'              => true,
+		'publish_posts'           => true,
+		'edit_published_posts'    => true,
+        'delete_posts'            => true,
+		'delete_published_posts'  => true,
+		'candidatar_oportunidade' => true,
+    ));
+	add_role( 'contratante', 'Contratante', array(
+		'read'                    => true,
+		'edit_posts'              => true,
+		'publish_posts'           => true,
+		'edit_published_posts'    => true,
+        'delete_posts'            => true,
+		'delete_published_posts'  => true,
+		'adicionar_oportunidade'  => true,
+	));
+
+	$editor = $wp_roles->get_role( 'editor' );
+	$moderador_caps = $editor->capabilities + [
+		'adicionar_lider'        => true,
+		'adicionar_contratante'  => true,
+		'adicionar_oportunidade' => true,
+	];
+	add_role( 'moderador', 'Moderador', $moderador_caps );
+
+	add_role( 'inativo', 'Inativo', [] );
+}
+
+add_action( 'init', 'lemann_roles_capabilities' );
