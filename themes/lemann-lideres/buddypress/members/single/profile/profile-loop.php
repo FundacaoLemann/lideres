6169b4fb31e4be3de8aba50ed3cc103c5a4d8337
Felipe Elia <felipe.elia@gmail.com>
2018-07-24 14:48:18 -0300

    [#16] Esconde campos do perfil que já são exibidos no cabeçalho

--- themes/lemann-lideres/buddypress/members/single/profile/profile-loop.php
+++ themes/lemann-lideres/buddypress/members/single/profile/profile-loop.php
@@ -9,7 +9,12 @@
 /** This action is documented in bp-templates/bp-legacy/buddypress/members/single/profile/profile-wp.php */
 do_action( 'bp_before_profile_loop_content' ); ?>
 
-<?php if ( bp_has_profile() ) : ?>
+<?php
+$args = [
+	'exclude_groups' => '1 4',
+	'exclude_fields' => '254 255 256',
+];
+if ( bp_has_profile( $args ) ) : ?>
 
 	<?php while ( bp_profile_groups() ) : bp_the_profile_group(); ?>
 
