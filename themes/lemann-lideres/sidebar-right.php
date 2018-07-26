4e0b326c31decbef097cc14337890ac7b64089dd
Tulipa Negra <donnakether@gmail.com>
2018-07-26 20:56:28 -0300

    Estilos e sidebar para pagina de perfil

---
+++ themes/lemann-lideres/sidebar-right.php
@@ -0,0 +1,339 @@
+<?php if ( $GLOBALS['ghostpool_layout'] == 'gp-both-sidebars' OR $GLOBALS['ghostpool_layout'] == 'gp-right-sidebar' ) { ?>
+
+	<aside id="gp-sidebar-right" class="gp-sidebar">
+	<div class="bp-widget disponibilidade-para-oportunidade">
+
+<h2>Disponibilidade para Oportunidade</h2>
+
+<table class="profile-fields">
+
+	
+		
+			<tbody><tr class="field_130 field_quando-teria-interesse-em-oportunidades-profissionais required-field visibility-public field_type_radio">
+
+				<td class="label">Quando teria interesse em oportunidades profissionais?</td>
+
+				<td class="data"><p><a href="https://lideres.hacklab.com.br/membros/?members_search=Imediato" rel="nofollow">Imediato</a></p>
+</td>
+
+			</tr>
+
+		
+		
+	
+		
+			<tr class="field_136 field_faixa-salarial-esperada optional-field visibility-public alt field_type_radio">
+
+				<td class="label">Faixa salarial esperada </td>
+
+				<td class="data"><p><a href="https://lideres.hacklab.com.br/membros/?members_search=Entre+5+e+7+mil" rel="nofollow">Entre 5 e 7 mil</a></p>
+</td>
+
+			</tr>
+
+		
+		
+	
+		
+			<tr class="field_143 field_citar-processo-seletivo-que-voce-ja-esta-participando-ou-organizacoes-que-voce-ja-teve-conversas-sobre-oportunidades-profissionais-recentemente optional-field visibility-public field_type_textarea">
+
+				<td class="label">Citar processo seletivo que você já está participando ou organizações que você já teve conversas sobre oportunidades profissionais recentemente </td>
+
+				<td class="data"><p>Folha de São Paulo; Veja; Valor Econômico</p>
+</td>
+
+			</tr>
+
+		
+		
+	
+		
+			<tr class="field_144 field_setores-de-interesse-para-trabalhar required-field visibility-public alt field_type_checkbox">
+
+				<td class="label">Setores de interesse para Trabalhar</td>
+
+				<td class="data"><p><a href="https://lideres.hacklab.com.br/membros/?members_search=Pedagogia+%2F+Licenciatura" rel="nofollow">Pedagogia / Licenciatura</a>, <a href="https://lideres.hacklab.com.br/membros/?members_search=Outros" rel="nofollow">Outros</a></p>
+</td>
+
+			</tr>
+
+		
+		
+	
+		
+			<tr class="field_260 field_outro-especifique optional-field visibility-public field_type_textbox">
+
+				<td class="label">Outro (especifique)</td>
+
+				<td class="data"><p>Jornalismo</p>
+</td>
+
+			</tr>
+
+		
+		
+	
+		
+			<tr class="field_163 field_temas-que-gostaria-de-trabalhar optional-field visibility-public alt field_type_checkbox">
+
+				<td class="label">Temas que gostaria de trabalhar </td>
+
+				<td class="data"><p><a href="https://lideres.hacklab.com.br/membros/?members_search=Pesquisa+Acad%C3%AAmica" rel="nofollow">Pesquisa Acadêmica</a>, <a href="https://lideres.hacklab.com.br/membros/?members_search=Gest%C3%A3o+P%C3%BAblica" rel="nofollow">Gestão Pública</a>, <a href="https://lideres.hacklab.com.br/membros/?members_search=Direitos+Humanos" rel="nofollow">Direitos Humanos</a>, <a href="https://lideres.hacklab.com.br/membros/?members_search=Ci%C3%AAncia" rel="nofollow">Ciência</a></p>
+</td>
+
+			</tr>
+
+		
+		
+	
+		
+			<tr class="field_261 field_outro-especifique optional-field visibility-public field_type_textbox">
+
+				<td class="label">Outro (especifique)</td>
+
+				<td class="data"><p>Tecnologia</p>
+</td>
+
+			</tr>
+
+		
+		
+	
+		
+			<tr class="field_179 field_organizacoes-que-gostaria-de-trabalhar required-field visibility-public alt field_type_textarea">
+
+				<td class="label">Organizações que Gostaria de Trabalhar </td>
+
+				<td class="data"><p>Ambev; Lemann</p>
+</td>
+
+			</tr>
+
+		
+		
+	
+		
+			<tr class="field_180 field_cidade-de-residencia-atual required-field visibility-public field_type_textbox">
+
+				<td class="label">Cidade de residência atual </td>
+
+				<td class="data"><p>São Paulo</p>
+</td>
+
+			</tr>
+
+		
+		
+	
+		
+			<tr class="field_181 field_disponibilidade-para-trabalhar-em required-field visibility-public alt field_type_checkbox">
+
+				<td class="label">Disponibilidade para trabalhar em:</td>
+
+				<td class="data"><p>Em outras cidades do estado que resido (abrir múltiplas opções), Em capitais ou grandes cidades do Brasil (abrir múltiplas opções)</p>
+</td>
+
+			</tr>
+
+		
+		
+	
+		
+			<tr class="field_190 field_disponibilidade-para-viagens-lista-com-selecao-unica-multiplas-selecoes-obrigatorios required-field visibility-public field_type_radio">
+
+				<td class="label">Disponibilidade para viagens (lista com seleção única / múltiplas seleções - obrigatórios)</td>
+
+				<td class="data"><p><a href="https://lideres.hacklab.com.br/membros/?members_search=Eventualmente+Nacionais" rel="nofollow">Eventualmente Nacionais</a></p>
+</td>
+
+			</tr>
+
+		
+		
+	
+</tbody></table>
+</div>
+<div class="bp-widget disponibilidade-para-oportunidade">
+
+				<h2>Disponibilidade para Oportunidade</h2>
+
+				<table class="profile-fields">
+
+					
+						
+							<tbody><tr class="field_130 field_quando-teria-interesse-em-oportunidades-profissionais required-field visibility-public field_type_radio">
+
+								<td class="label">Quando teria interesse em oportunidades profissionais?</td>
+
+								<td class="data"><p><a href="https://lideres.hacklab.com.br/membros/?members_search=Imediato" rel="nofollow">Imediato</a></p>
+</td>
+
+							</tr>
+
+						
+						
+					
+						
+							<tr class="field_136 field_faixa-salarial-esperada optional-field visibility-public alt field_type_radio">
+
+								<td class="label">Faixa salarial esperada </td>
+
+								<td class="data"><p><a href="https://lideres.hacklab.com.br/membros/?members_search=Entre+5+e+7+mil" rel="nofollow">Entre 5 e 7 mil</a></p>
+</td>
+
+							</tr>
+
+						
+						
+					
+						
+							<tr class="field_143 field_citar-processo-seletivo-que-voce-ja-esta-participando-ou-organizacoes-que-voce-ja-teve-conversas-sobre-oportunidades-profissionais-recentemente optional-field visibility-public field_type_textarea">
+
+								<td class="label">Citar processo seletivo que você já está participando ou organizações que você já teve conversas sobre oportunidades profissionais recentemente </td>
+
+								<td class="data"><p>Folha de São Paulo; Veja; Valor Econômico</p>
+</td>
+
+							</tr>
+
+						
+						
+					
+						
+							<tr class="field_144 field_setores-de-interesse-para-trabalhar required-field visibility-public alt field_type_checkbox">
+
+								<td class="label">Setores de interesse para Trabalhar</td>
+
+								<td class="data"><p><a href="https://lideres.hacklab.com.br/membros/?members_search=Pedagogia+%2F+Licenciatura" rel="nofollow">Pedagogia / Licenciatura</a>, <a href="https://lideres.hacklab.com.br/membros/?members_search=Outros" rel="nofollow">Outros</a></p>
+</td>
+
+							</tr>
+
+						
+						
+					
+						
+							<tr class="field_260 field_outro-especifique optional-field visibility-public field_type_textbox">
+
+								<td class="label">Outro (especifique)</td>
+
+								<td class="data"><p>Jornalismo</p>
+</td>
+
+							</tr>
+
+						
+						
+					
+						
+							<tr class="field_163 field_temas-que-gostaria-de-trabalhar optional-field visibility-public alt field_type_checkbox">
+
+								<td class="label">Temas que gostaria de trabalhar </td>
+
+								<td class="data"><p><a href="https://lideres.hacklab.com.br/membros/?members_search=Pesquisa+Acad%C3%AAmica" rel="nofollow">Pesquisa Acadêmica</a>, <a href="https://lideres.hacklab.com.br/membros/?members_search=Gest%C3%A3o+P%C3%BAblica" rel="nofollow">Gestão Pública</a>, <a href="https://lideres.hacklab.com.br/membros/?members_search=Direitos+Humanos" rel="nofollow">Direitos Humanos</a>, <a href="https://lideres.hacklab.com.br/membros/?members_search=Ci%C3%AAncia" rel="nofollow">Ciência</a></p>
+</td>
+
+							</tr>
+
+						
+						
+					
+						
+							<tr class="field_261 field_outro-especifique optional-field visibility-public field_type_textbox">
+
+								<td class="label">Outro (especifique)</td>
+
+								<td class="data"><p>Tecnologia</p>
+</td>
+
+							</tr>
+
+						
+						
+					
+						
+							<tr class="field_179 field_organizacoes-que-gostaria-de-trabalhar required-field visibility-public alt field_type_textarea">
+
+								<td class="label">Organizações que Gostaria de Trabalhar </td>
+
+								<td class="data"><p>Ambev; Lemann</p>
+</td>
+
+							</tr>
+
+						
+						
+					
+						
+							<tr class="field_180 field_cidade-de-residencia-atual required-field visibility-public field_type_textbox">
+
+								<td class="label">Cidade de residência atual </td>
+
+								<td class="data"><p>São Paulo</p>
+</td>
+
+							</tr>
+
+						
+						
+					
+						
+							<tr class="field_181 field_disponibilidade-para-trabalhar-em required-field visibility-public alt field_type_checkbox">
+
+								<td class="label">Disponibilidade para trabalhar em:</td>
+
+								<td class="data"><p>Em outras cidades do estado que resido (abrir múltiplas opções), Em capitais ou grandes cidades do Brasil (abrir múltiplas opções)</p>
+</td>
+
+							</tr>
+
+						
+						
+					
+						
+							<tr class="field_190 field_disponibilidade-para-viagens-lista-com-selecao-unica-multiplas-selecoes-obrigatorios required-field visibility-public field_type_radio">
+
+								<td class="label">Disponibilidade para viagens (lista com seleção única / múltiplas seleções - obrigatórios)</td>
+
+								<td class="data"><p><a href="https://lideres.hacklab.com.br/membros/?members_search=Eventualmente+Nacionais" rel="nofollow">Eventualmente Nacionais</a></p>
+</td>
+
+							</tr>
+
+						
+						
+					
+				</tbody></table>
+			</div>
+	<div class="bp-widget contribuicao-apoio-para-a-rede-de-lideres-lemann">
+
+<h2>Contribuição/Apoio para a Rede de Lideres Lemann</h2>
+
+<table class="profile-fields">
+
+	
+		
+			<tbody><tr class="field_202 field_apoio-de-co-construcao-da-rede-de-lideres-lemann required-field visibility-public field_type_checkbox">
+
+				<td class="label">Apoio de Co-Construção da Rede de Líderes Lemann</td>
+
+				<td class="data"><p>Conversa com time da FL por telefone ou call</p>
+</td>
+
+			</tr>
+
+		
+		
+	
+</tbody></table>
+</div>
+	
+		<?php get_template_part( 'lib/sections/sensei/sensei-details' ); ?>
+	
+		<?php if ( is_active_sidebar( $GLOBALS['ghostpool_right_sidebar'] ) ) {
+			dynamic_sidebar( $GLOBALS['ghostpool_right_sidebar'] );
+		} ?>		
+
+	</aside>
+
+<?php } ?>
