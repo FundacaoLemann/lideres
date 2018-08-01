23e033a6ece35057ad1bcefc69df229e49bd0e40
Felipe Elia <felipe.elia@gmail.com>
2018-07-31 22:13:10 -0300

    [#34] Remove a barra de administração do WP para usuário não-admin

--- plugins/bp-custom.php
+++ plugins/bp-custom.php
@@ -14,3 +14,13 @@ function lemann_bp_setup_nav() {
 	bp_core_remove_nav_item( 'events' );
 }
 add_action( 'bp_setup_nav', 'lemann_bp_setup_nav', 999 );
+
+/**
+ * Remove a barra do WordPress se o usuário não for um superadmin.
+ */
+function lemann_remove_admin_bar() {
+	if ( ! is_super_admin() ) {
+		add_filter( 'show_admin_bar', '__return_false' );
+	}
+}
+add_action( 'wp', 'lemann_remove_admin_bar' );
