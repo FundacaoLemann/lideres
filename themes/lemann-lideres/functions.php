85eb61a23d986fdcad073c43984b6585b71da31a
Henrique Marques <henrique@hacklab.com.br>
2018-08-01 15:54:57 -0300

    Displaying error messages on failed login attempt

--- themes/lemann-lideres/functions.php
+++ themes/lemann-lideres/functions.php
@@ -19,3 +19,22 @@ add_action( 'wp_enqueue_scripts', function () {
 
     wp_enqueue_style( 'js_composer_front' );
 });
+
+add_filter('login_redirect', 'my_login_redirect', 10, 3);
+function my_login_redirect($redirect_to, $requested_redirect_to, $user) {
+    if (is_wp_error($user)) {
+        session_start();
+        $error_types = array_keys($user->errors);
+        $error_type = 'both_empty';
+        if (is_array($error_types) && !empty($error_types)) {
+            $error_type = $error_types[0];
+        }
+
+        $_SESSION["login_error"] = $user->get_error_message();
+
+        wp_redirect( get_permalink( 93 ) . "/login?login=failed&reason=" . $error_type );
+        exit;
+    } else {
+        return home_url();
+    }
+}
