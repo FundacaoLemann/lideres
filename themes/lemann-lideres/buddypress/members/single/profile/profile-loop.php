5a3001e4c770111c61778ecd5f17edf90bf4728d
Felipe Elia <felipe.elia@gmail.com>
2018-08-09 13:50:31 -0300

    [#95] Exibe cargo e organização no "corpo" do perfil

--- themes/lemann-lideres/buddypress/members/single/profile/profile-loop.php
+++ themes/lemann-lideres/buddypress/members/single/profile/profile-loop.php
@@ -10,14 +10,17 @@
 do_action( 'bp_before_profile_loop_content' ); ?>
 
 <?php
-$exclude_groups = [ 1, 4, 9, 10, 11 ];
+$exclude_groups = [ 4, 9, 10, 11 ];
 if ( is_event_month() ) {
 	$exclude_groups[] = 8;
 }
 
+// Campos do grupo com id 1 que não devem ser exibidos.
+$campos_destaque_perfil = [ 1, 253, 27, 32, 574 ];
+
 $args = [
 	'exclude_groups' => implode( ',', $exclude_groups ),
-	'exclude_fields' => '80 254 255 256 ' . implode( ' ', LEMANN_BP_OUTROS ),
+	'exclude_fields' => '80 254 255 256 ' . implode( ' ', LEMANN_BP_OUTROS + $campos_destaque_perfil ),
 ];
 if ( bp_has_profile( $args ) ) {
 	bp_get_template_part( 'members/single/profile/profile-group-loop' );
