64c895bdc5f9e1d8e6c0817841c1eab14e65e745
Felipe Elia <felipe.elia@gmail.com>
2018-08-23 14:48:33 -0300

    [#100] Home: altera mensagem antes do form de post

--- themes/lemann-lideres/buddypress/activity/post-form.php
+++ themes/lemann-lideres/buddypress/activity/post-form.php
@@ -32,7 +32,7 @@
 		 * @see https://github.com/buddypress/BuddyPress/blob/master/src/bp-templates/bp-legacy/buddypress/activity/post-form.php
 		 */
 		printf(
-			'%s, compartilhe sua opinião e participe de Rede de Líderes',
+			'%s, compartilhe sua opinião e participe da Rede',
 			bp_get_user_firstname( bp_get_loggedin_user_fullname() )
 		);
 		?>
