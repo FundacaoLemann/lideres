4fb01149172e9a86cb017c1a87b24ba018730cfe
Felipe Elia <felipe.elia@gmail.com>
2018-09-04 23:21:09 -0300

    [#9 e #143] Match: começa implementação do envio do e-mail

--- themes/lemann-lideres/includes/match/index.php
+++ themes/lemann-lideres/includes/match/index.php
@@ -18,18 +18,24 @@ define( 'LEMANN_VAGAS_DE_PARA', [
 
 define( 'LEMANN_MATCHES_META_KEY', 'job_listings_matches' );
 
+define( 'LEMANN_MATCH_MINIMO_EMAIL', 70 );
+
 /**
  * Define a porcentagem de correspondência entre uma vaga e um usuário.
  *
- * @param int $post_id ID da vaga.
- * @param int $user_id ID do usuário.
+ * @param int  $post_id ID da vaga.
+ * @param int  $user_id ID do usuário.
+ * @param bool $send_email Se deve ou não enviar um e-mail para o usuário.
  */
-function lemann_match( $post_id, $user_id ) {
+function lemann_match( $post_id, $user_id, $send_email = false ) {
+	set_time_limit( 5 );
+
 	$possible_matches = 0;
 	$real_matches     = 0;
 
 	foreach ( LEMANN_VAGAS_DE_PARA as $wpjm_id => $bp_id ) {
 		$job_listing_data = get_post_meta( $post_id, "_{$wpjm_id}", true );
+
 		if ( 'graduacao' == $wpjm_id ) {
 			$user_data = BP_XProfile_ProfileData::get_value_byid( $bp_id, $user_id );
 			$user_data = Lemann_Field_Graduacao::unserialize( $user_data );
@@ -52,7 +58,7 @@ function lemann_match( $post_id, $user_id ) {
 
 			case 'graduacao':
 				$possible_matches++;
-				if ( is_array( $user_data ) ) {
+				if ( is_array( $user_data ) && ! empty( $user_data[0] ) ) {
 					foreach ( $user_data as $graduacao ) {
 						if ( $graduacao['nivel'] == $job_listing_data ) {
 							$real_matches++;
@@ -73,6 +79,29 @@ function lemann_match( $post_id, $user_id ) {
 
 	$match = ( $real_matches / $possible_matches ) * 100;
 
+	// To Do: Tirar esse teste com o ID do usuário.
+	if ( 1 == $user_id ) {
+		if ( $send_email && $match >= LEMANN_MATCH_MINIMO_EMAIL ) {
+			$user = get_user_by( 'id', $user_id );
+			wp_mail(
+				$user->user_email,
+				__( 'Nova vaga no Portal de Líderes da Fundação Lemann', 'lemann-lideres' ),
+				sprintf(
+					__(
+						'<p>Temos uma nova vaga no Portal de Líderes da Fundação Lemann:</p>' .
+						'<p><strong>%1$s</strong> na %2$s</p>' .
+						'<p>Acesse: <a href="%3$s">%3$s</a></p>',
+						'lemann-lideres'
+					),
+					get_the_title( $post_id ),
+					get_the_company_name( $post_id ),
+					get_the_permalink( $post_id )
+				),
+				[ 'Content-Type: text/html; charset=UTF-8' ]
+			);
+		}
+	}
+
 	$matches = (array) get_user_meta( $user_id, LEMANN_MATCHES_META_KEY, true );
 	$matches[ $post_id ] = [
 		'match' => $match,
@@ -88,11 +117,11 @@ function lemann_match( $post_id, $user_id ) {
 add_action( 'save_post_job_listing', function( $post_id ) {
 	if ( 'publish' == get_post_status( $post_id ) ) {
 		$users = get_users( [
-			'role'   => 'lider',
-			'fields' => 'ID',
+			'role__in' => [ 'lider', 'administrator' ],
+			'fields'   => 'ID',
 		] );
 		foreach ( $users as $user_id ) {
-			lemann_match( $post_id, $user_id );
+			lemann_match( $post_id, $user_id, true );
 		}
 	}
 } );
