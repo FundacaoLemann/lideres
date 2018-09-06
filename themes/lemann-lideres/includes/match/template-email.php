eb5e7cf56ca908a8d20eb95a7469451d93b631e0
Leonardo <leopiccionia@gmail.com>
2018-09-06 13:58:50 -0300

    New template for match email.

--- themes/lemann-lideres/includes/match/template-email.php
+++ themes/lemann-lideres/includes/match/template-email.php
@@ -9,7 +9,7 @@
             <p style="margin-left: 20%; padding-bottom: 15px; width: 60%;">Nosso banco de oportunidades encontrou uma vaga de alta compatibilidade com seu perfil. Confira as condições da vaga e boa sorte!</p>
         </div>
         <div style="background: white; padding: 30px;">
-            <div style="border: 1px solid #EEE; margin-left: 20%; padding: 20px; position: relative; width: 60%;">
+            <div style="border: 1px solid #EEE; margin: auto; padding: 20px; position: relative; width: 60%;">
                 <div style="background: #F65; border-radius: 5px; color: white; max-width: 100px; padding: 4px 8px; position: absolute; right: 10px; top: 10px;">Match <strong>{{ match }}%</strong></div>
                 <img src="{{ company_logo }}" alt="{{ company_name }}" style="border: 4px solid #E7E7E7; border-radius: 50%; height: 100px; width: 100px;">
                 <p style="color: #222; font-weight: bold;">{{ job_title }}</p>
@@ -19,12 +19,12 @@
                 </div>
             </div>
             <div>
-                <p style="padding-top: 15px;"><a href="{{ job_url }}" style="color: #083050; font-weight: bold; text-decoration: none;">Veja mais detalhes sobre esta vaga</a></p>
+                <p style="padding-top: 20px;"><a href="{{ job_url }}" style="background: #083050; border-radius: 5px; color: white; padding: 10px 20px; text-decoration: none;">Veja mais detalhes sobre esta vaga</a></p>
             </div>
         </div>
         <footer>
             <p style="padding: 20px 0 40px;">
-                <a href="https://lideres.fundacaolemann.org.br/vagas/" style="background: #083050; border-radius: 5px; color: white; padding: 10px 20px; text-decoration: none;">Confira outras oportunidades</a>
+                <a href="https://lideres.fundacaolemann.org.br/vagas/" style="color: #083050; font-weight: bold; text-decoration: none;">Confira outras oportunidades</a>
             </p>
         </footer>
     </div>
