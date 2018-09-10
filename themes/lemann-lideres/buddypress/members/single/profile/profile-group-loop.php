a98507ff41c994a4eac4153d15bea0a2096ef3ef
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2018-09-10 20:33:50 -0300

    caixa carreira com texto 'Você ainda não publicou nada neste campo' quando o usuário não preencheu nenhum campo de carreira

--- themes/lemann-lideres/buddypress/members/single/profile/profile-group-loop.php
+++ themes/lemann-lideres/buddypress/members/single/profile/profile-group-loop.php
@@ -1,6 +1,18 @@
-<?php while ( bp_profile_groups() ) : bp_the_profile_group(); ?>
+<?php
 
-	<?php if ( bp_profile_group_has_fields() ) : ?>
+if(!defined('CARREIRA_GROUP_ID')){
+    define('CARREIRA_GROUP_ID', 14);
+}
+$carreira_empty = true;
+while ( bp_profile_groups() ) : bp_the_profile_group(); ?>
+
+    <?php 
+    if ( bp_profile_group_has_fields() ) : 
+        global $group;
+        if($group->id == CARREIRA_GROUP_ID){
+            $carreira_empty = false;
+        }
+        ?>
 
 		<?php
 		/** This action is documented in bp-templates/bp-legacy/buddypress/members/single/profile/profile-wp.php */
@@ -16,7 +28,7 @@
 
 					<?php if ( bp_field_has_data() ) : ?>
 
-						<tr<?php bp_field_css_class(); ?>>
+						<tr <?php bp_field_css_class(); ?>>
 
 							<td class="label"><?php bp_the_profile_field_name(); ?></td>
 
@@ -46,6 +58,16 @@
 	<?php endif; ?>
 
 <?php endwhile; ?>
+
+<?php if($carreira_empty && (bp_displayed_user_id() == get_current_user_id())): ?>
+<div class="bp-widget carreira empty-carreira">
+    <h2>Carreira</h2>
+    <div>
+        <p>Você ainda não publicou nada neste campo</p>
+        <p><a class="btn" href="./profile/edit/group/<?php echo CARREIRA_GROUP_ID ?>/">Editar Carreira</a></p>
+    </div>
+</div>
+<?php endif; ?>
 
 <?php
 /** This action is documented in bp-templates/bp-legacy/buddypress/members/single/profile/profile-wp.php */
