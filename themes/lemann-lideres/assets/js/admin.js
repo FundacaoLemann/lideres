a8bfab76e25075b0b061aaf6e413a46a760b7a86
Leonardo <leopiccionia@gmail.com>
2019-06-06 14:45:08 -0300

    Preview oportunidades in parent frame.

--- themes/lemann-lideres/assets/js/admin.js
+++ themes/lemann-lideres/assets/js/admin.js
@@ -2,4 +2,8 @@ jQuery(function(){
     setTimeout(function(){
         jQuery('#_job_expires').datepicker( "option", "dateFormat", 'dd/mm/yy' );
     },2000);
+
+    if (window.parent) {
+        jQuery('.components-notice.is-success a').attr('target', '_parent');
+    }
 });
