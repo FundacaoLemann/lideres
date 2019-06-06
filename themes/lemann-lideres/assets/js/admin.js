cab18becd0d03af975c73812413b3fdf98c5d70f
Leonardo <leopiccionia@gmail.com>
2019-06-06 18:26:01 -0300

    Revert "Preview oportunidades in parent frame."
    This reverts commit f8e23f5f7d74003ac4c73bb6551046d7c5f9c572.

--- themes/lemann-lideres/assets/js/admin.js
+++ themes/lemann-lideres/assets/js/admin.js
@@ -2,8 +2,4 @@ jQuery(function(){
     setTimeout(function(){
         jQuery('#_job_expires').datepicker( "option", "dateFormat", 'dd/mm/yy' );
     },2000);
-
-    if (window.parent) {
-        jQuery('.components-notice.is-success a').attr('target', '_parent');
-    }
 });
