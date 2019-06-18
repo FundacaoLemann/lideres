6c8030d51329d6992994dc53bd6971697d961617
Leonardo <leopiccionia@gmail.com>
2019-06-18 18:13:27 -0300

    Hide state and city fields if country different from Brazil.

--- themes/lemann-lideres/assets/js/lemann-search-profiles.js
+++ themes/lemann-lideres/assets/js/lemann-search-profiles.js
@@ -8,3 +8,19 @@ var profileFiltersVM = new Vue({
         }
     }
 });
+
+jQuery(function () {
+    var countrySelect = document.querySelector('select#field_812');
+    var stateField = document.querySelector('.field_813');
+    var cityField = document.querySelector('.field_814');
+
+    countrySelect.addEventListener('change', function () {
+        if (countrySelect.value === 'Brasil' || countrySelect.value === '') {
+            stateField.style.display = '';
+            cityField.style.display = '';
+        } else {
+            stateField.style.display = 'none';
+            cityField.style.display = 'none';
+        }
+    });
+});
